ba7818cb62fbff04971cbc5c6436598581fb5a54
Terry Threatt <terry.threatt@gmail.com>
2020-03-18 14:18:40 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,18 @@
 require 'nokogiri'
 require 'open-uri'
 
+# flatiron html 
 html = open("https://flatironschool.com/")
+
+# coverting strings of html to node 
+doc = Nokogiri::HTML(html)
+
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+ 
+courses.each do |course|
+  puts course.text.strip
+end
+
+p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].name
+
+p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].attributes
